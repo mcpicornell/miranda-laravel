51452d6e5d01b4b9e2f5632fce6a20dfc01a00db
Ubuntu <ubuntu@ip-172-31-23-76.eu-west-3.compute.internal>
2023-07-16 14:45:02 +0000

    changes on migration of orders

--- database/migrations/2023_07_16_143240_create_orders_table.php
+++ database/migrations/2023_07_16_143240_create_orders_table.php
@@ -1,29 +1,36 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_guest_id');
-            $table->unsignedBigInteger('roomId');
+            $table->unsignedBigInteger('roomNumber');
 
             $table->string('type');
             $table->text('description');
             $table->timestamps();
 
-
             $table->foreign('user_guest_id')->references('id')->on('users_guests')->onDelete('cascade');
-            $table->foreign('roomId')->references('id')->on('rooms')->onDelete('cascade');
+            $table->foreign('roomNumber')->references('roomNumber')->on('rooms')->onDelete('cascade');
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down(): void
     {
         Schema::dropIfExists('orders');
     }
-}
+};
