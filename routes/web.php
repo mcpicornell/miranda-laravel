57f5052520ed62d3c172367ddf45565ea6395df0
Marc <marc.castro.picornell@gmail.com>
2023-07-20 21:15:29 +0200

    route dashboard deleted

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/orders', [OrdersController::class, 'index'])->name('orders.index');
-    Route::get('/dashboard', [OrdersController::class, 'index'])->name('orders.index');
     Route::post('/new-order', [OrdersController::class, 'store'])->name('orders.store');
     Route::get('/new-order', [OrdersController::class, 'newOrder'])->name('newOrder.newOrder');
 
